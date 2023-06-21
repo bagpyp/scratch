da924d491feef774f95bd70488b119996848c83a
robertcunningham <robertcunningham@ford.com>
2023-06-21 09:48:09 -0700

    error boundary

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function PropDrilling() {
-  const [count] = useState(0)
-  return <Child count={count} />
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  // to detect an error and update the state?
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Bah!', error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Fallback UI</h1>
+    }
+    return this.props.children
+  }
 }
 
-function Child({ count }) {
-  return <Grandchild count={count} />
-}
-
-function Grandchild({ count }) {
-  return <div>{count}</div>
+function Component() {
+  return <p>A Component {x}</p>
 }
 
 function App() {
   return (
     <>
-      <PropDrilling />
+      <h1>Hi</h1>
+      <ErrorBoundary>
+        <Component />
+      </ErrorBoundary>
     </>
   )
 }
